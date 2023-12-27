4c3cda174d6f6024cd84bfea999de7893a0a3be0
Vitor45QW <joao8816@hotmail.com>
2023-12-27 04:27:26 -0300

    Add Learn faster project folder

---
+++ js/index.js
@@ -0,0 +1,24 @@
+function wantToLearn () {
+
+
+    alert("Pratice. Pratice");
+
+}
+
+function displayLinks () {
+
+
+    let link1 = document.getElementById('link1');
+
+    link1.innerHTML = '<div class="div-links"> <a class="javascript"href="https://www.youtube.com/watch?v=BXqUH86F-kA&list=PLntvgXM11X6pi7mW0O4ZmfUI1xDSIbmTm" target="_blank"> JavaScript solid basis</a> </div>';
+
+    
+}
+
+function displayLinks2 () {
+
+    let link2 = document.getElementById('link2');
+
+    link2.innerHTML ='<div class="header2"> <a href="https://www.youtube.com/watch?v=UBAX-13g8OM">Git solid basis</a> </div>';
+
+}
